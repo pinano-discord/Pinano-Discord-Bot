406611c42d32ffb51c5908872150d75588a76b83
Emil Sit <sit@emilsit.net>
2019-01-26 23:08:27 -0500

    Fix all violations of camelcase rule
    Rename variables to use camelCase instead of snake_case.

--- library/leaderboard_fetch.js
+++ library/leaderboard_fetch.js
@@ -7,28 +7,28 @@ module.exports = (client, db) => {
     */
 
   client.getOverallLeaderboard = (message, callback) => {
-    let score_array = []
+    let scoreArray = []
     let msgStr = ''
     db.collection('users').find({}).toArray()
       .then(async res => {
         await res.forEach(doc => {
           let string = `${doc.id}|${doc.overall_session_playtime}`
-          score_array.push(string)
+          scoreArray.push(string)
         })
         await con()
       })
     function con () {
-      score_array.sort((a, b) => {
+      scoreArray.sort((a, b) => {
         a = a.split('|').splice(1).join('')
         b = b.split('|').splice(1).join('')
         return b - a
       })
       for (let j = 0; j < client.settings.leaderboard_size; j++) {
-        if (!score_array[j]) {
+        if (!scoreArray[j]) {
           msgStr += ``
         } else {
-          let user = score_array[j].split('|')[0]
-          let time = client.hd(score_array[j].split('|')[1] * 1000, { units: ['h', 'm', 's'] })
+          let user = scoreArray[j].split('|')[0]
+          let time = client.hd(scoreArray[j].split('|')[1] * 1000, { units: ['h', 'm', 's'] })
           if (client.users.get(user)) {
             msgStr += `**${j + 1}. ${client.users.get(user).username}#${client.users.get(user).discriminator}**\n \`${time}\`\n`
           } else {
@@ -45,28 +45,28 @@ module.exports = (client, db) => {
   }
 
   client.getWeeklyLeaderboard = (message, callback) => {
-    let score_array = []
+    let scoreArray = []
     let msgStr = ''
     db.collection('users').find({}).toArray()
       .then(async res => {
         await res.forEach(doc => {
           let string = `${doc.id}|${doc.current_session_playtime}`
-          score_array.push(string)
+          scoreArray.push(string)
         })
         await con()
       })
     function con () {
-      score_array.sort((a, b) => {
+      scoreArray.sort((a, b) => {
         a = a.split('|').splice(1).join('')
         b = b.split('|').splice(1).join('')
         return b - a
       })
       for (let j = 0; j < client.settings.leaderboard_size; j++) {
-        if (!score_array[j]) {
+        if (!scoreArray[j]) {
           msgStr += ``
         } else {
-          let user = score_array[j].split('|')[0]
-          let time = client.hd(score_array[j].split('|')[1] * 1000, { units: ['h', 'm', 's'] })
+          let user = scoreArray[j].split('|')[0]
+          let time = client.hd(scoreArray[j].split('|')[1] * 1000, { units: ['h', 'm', 's'] })
           if (client.users.get(user)) {
             msgStr += `**${j + 1}. ${client.users.get(user).username}#${client.users.get(user).discriminator}**\n \`${time}\`\n`
           } else {
@@ -87,68 +87,68 @@ module.exports = (client, db) => {
   }
 
   client.fetchWeeklyLeaderboardPos = (message, callback) => {
-    let score_array = []
+    let scoreArray = []
     let msgStr = ''
     db.collection('users').find({}).toArray()
       .then(async res => {
         await res.forEach(doc => {
           let string = `${doc.id}|${doc.current_session_playtime}`
-          score_array.push(string)
+          scoreArray.push(string)
         })
         await con()
       })
     function con () {
-      score_array.sort((a, b) => {
+      scoreArray.sort((a, b) => {
         a = a.split('|').splice(1).join('')
         b = b.split('|').splice(1).join('')
         return b - a
       })
 
       let pos = 0
-      let true_j = 0
-      for (let i = 0; i <= score_array.length; i++) {
-        if (score_array[i]) {
-          let user = score_array[i].split('|')[0]
-          if (user === message.author.id) { pos = true_j + 1 }
-          true_j++
+      let trueJ = 0
+      for (let i = 0; i <= scoreArray.length; i++) {
+        if (scoreArray[i]) {
+          let user = scoreArray[i].split('|')[0]
+          if (user === message.author.id) { pos = trueJ + 1 }
+          trueJ++
         }
       }
 
-      msgStr = `\`${pos}\` / \`${true_j}\``
+      msgStr = `\`${pos}\` / \`${trueJ}\``
 
       callback(msgStr)
     }
   }
 
   client.fetchOverallLeaderboardPos = (message, callback) => {
-    let score_array = []
+    let scoreArray = []
     let msgStr = ''
     db.collection('users').find({}).toArray()
       .then(async res => {
         await res.forEach(doc => {
           let string = `${doc.id}|${doc.overall_session_playtime}`
-          score_array.push(string)
+          scoreArray.push(string)
         })
         await con()
       })
     function con () {
-      score_array.sort((a, b) => {
+      scoreArray.sort((a, b) => {
         a = a.split('|').splice(1).join('')
         b = b.split('|').splice(1).join('')
         return b - a
       })
 
       let pos = 0
-      let true_j = 0
-      for (let i = 0; i <= score_array.length; i++) {
-        if (score_array[i]) {
-          let user = score_array[i].split('|')[0]
-          if (user === message.author.id) { pos = true_j + 1 }
-          true_j++
+      let trueJ = 0
+      for (let i = 0; i <= scoreArray.length; i++) {
+        if (scoreArray[i]) {
+          let user = scoreArray[i].split('|')[0]
+          if (user === message.author.id) { pos = trueJ + 1 }
+          trueJ++
         }
       }
 
-      msgStr = `\`${pos}\` / \`${true_j}\``
+      msgStr = `\`${pos}\` / \`${trueJ}\``
 
       callback(msgStr)
     }
