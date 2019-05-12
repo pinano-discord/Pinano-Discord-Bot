6deda37cd98773abe6552b4a4b8f898d1cb270dd
Eugene Chan <theproject@euge.ca>
2019-05-12 13:17:04 -0400

    Eliminate callbacks from client_events.js

--- commands/general/stats.js
+++ commands/general/stats.js
@@ -4,7 +4,7 @@ const moment = require('moment')
 
 module.exports.load = (client) => {
   client.commands['stats'] = {
-    run (message) {
+    async run (message) {
       let args = message.content.split(' ').splice(1)
       let username
       let discriminator
@@ -41,81 +41,80 @@ module.exports.load = (client) => {
         }
       }
 
-      client.loadUserData(userId, async res => {
-        if (res === null) {
-          client.errorMessage(message, 'Error fetching your data from our servers, please try again.')
-          return
-        }
+      let userInfo = await client.loadUserData(userId)
+      if (userInfo === null) {
+        client.errorMessage(message, 'Error fetching your data from our servers, please try again.')
+        return
+      }
 
-        // set "semi-global" variables
-        let avatar
-        let source
-        let poss
+      // set "semi-global" variables
+      let avatar
+      let source
+      let poss
 
-        // get leaderboard pos
-        await client.fetchWeeklyLeaderboardPos(message.guild.id, userId, pos => {
-          poss = pos.replace(/`/g, '')
+      // get leaderboard pos
+      await client.fetchWeeklyLeaderboardPos(message.guild.id, userId, pos => {
+        poss = pos.replace(/`/g, '')
+      })
+
+      // load template
+      await jimp.read('./assets/time_card.png')
+        .then(i => {
+          source = i
         })
 
-        // load template
-        await jimp.read('./assets/time_card.png')
-          .then(i => {
-            source = i
+      // overlay avatar on template image
+      await jimp.read(av)
+        .then(i => {
+          avatar = i
+        })
+      await avatar.resize(98, 98)
+      await source.composite(avatar, 14, 14)
+
+      // check if the user is actively pracking and update times live if necessary
+      let activeTime = 0
+      let guildInfo = await client.loadGuildData(message.guild.id)
+      // these last two conditions should be equivalent but maybe they were already pracking when the bot came up
+      if (mem.voiceChannel != null && guildInfo.permitted_channels.includes(mem.voiceChannel.id) && !mem.mute && mem.s_time != null) {
+        activeTime = moment().unix() - mem.s_time
+      }
+
+      // write the text stuff
+      await jimp.loadFont(jimp.FONT_SANS_16_WHITE)
+        .then(async font => {
+          source.print(font, 245, 25, `${username}#${discriminator}`)
+          source.print(font, 135, 90, abbreviateTime(userInfo.current_session_playtime + activeTime))
+          source.print(font, 280, 90, abbreviateTime(userInfo.overall_session_playtime + activeTime))
+          source.print(font, 435, 90, poss)
+        })
+
+      // send the pic as png
+      await source.getBufferAsync(jimp.MIME_PNG)
+        .then(buffer => {
+          message.channel.send({
+            files: [{
+              attachment: buffer,
+              name: 'level.jpg'
+            }]
           })
 
-        // overlay avatar on template image
-        await jimp.read(av)
-          .then(i => {
-            avatar = i
-          })
-        await avatar.resize(98, 98)
-        await source.composite(avatar, 14, 14)
-
-        // check if the user is actively pracking and update times live if necessary
-        let activeTime = 0
-        let guild = await client.loadGuildData(message.guild.id)
-        // these last two conditions should be equivalent but maybe they were already pracking when the bot came up
-        if (mem.voiceChannel != null && guild.permitted_channels.includes(mem.voiceChannel.id) && !mem.mute && mem.s_time != null) {
-          activeTime = moment().unix() - mem.s_time
-        }
-
-        // write the text stuff
-        await jimp.loadFont(jimp.FONT_SANS_16_WHITE)
-          .then(async font => {
-            source.print(font, 245, 25, `${username}#${discriminator}`)
-            source.print(font, 135, 90, abbreviateTime(res.current_session_playtime + activeTime))
-            source.print(font, 280, 90, abbreviateTime(res.overall_session_playtime + activeTime))
-            source.print(font, 435, 90, poss)
-          })
-
-        // send the pic as png
-        await source.getBufferAsync(jimp.MIME_PNG)
-          .then(buffer => {
-            message.channel.send({
-              files: [{
-                attachment: buffer,
-                name: 'level.jpg'
-              }]
+            // delete response after set time
+            .then(m => {
+              setTimeout(() => {
+                m.delete()
+              }, client.settings.res_destruct_time * 1000)
             })
+        })
 
-              // delete response after set time
-              .then(m => {
-                setTimeout(() => {
-                  m.delete()
-                }, client.settings.res_destruct_time * 1000)
-              })
-          })
-
-        function abbreviateTime (playtime) {
-          return hd(playtime * 1000, { units: ['h', 'm', 's'], round: true })
-            .replace('hours', 'h')
-            .replace('minutes', 'm')
-            .replace('seconds', 's')
-            .replace('hour', 'h')
-            .replace('minute', 'm')
-            .replace('second', 's')
-        }
-      })
+      function abbreviateTime (playtime) {
+        return hd(playtime * 1000, { units: ['h', 'm', 's'], round: true })
+          .replace('hours', 'h')
+          .replace('minutes', 'm')
+          .replace('seconds', 's')
+          .replace('hour', 'h')
+          .replace('minute', 'm')
+          .replace('second', 's')
+      }
     }
   }
 }
--- library/client_events.js
+++ library/client_events.js
@@ -31,7 +31,7 @@ module.exports = client => {
     }
 
     let user = await client.userRepository.load(message.author.id)
-    if (user === null) {
+    if (user == null) {
       user = client.makeUser(message.author.id)
       await client.userRepository.save(user)
       client.log(`User created for ${message.author.username}#${message.author.discriminator}`)
@@ -41,59 +41,57 @@ module.exports = client => {
     setTimeout(() => message.delete(), client.settings.req_destruct_time * 1000)
   })
 
-  client.on('guildMemberAdd', mem => {
-    if (mem.guild === null) {
+  client.on('guildMemberAdd', async (mem) => {
+    if (mem.guild == null) {
       return
     }
 
-    client.loadGuildData(mem.guild.id, res => {
-      if (res === null) {
-        return
-      }
+    let guildInfo = await client.loadGuildData(mem.guild.id)
+    if (guildInfo == null) {
+      return
+    }
 
-      if (res.dm_welcome_toggle === true && res.dm_welcome_message !== '') {
-        let msg = new client.discord.RichEmbed()
-        msg.setTitle('Welcome!')
-        msg.setDescription(res.dm_welcome_message)
-        msg.setColor(client.settings.embed_color)
-        msg.setTimestamp()
-        try {
-          mem.send(msg)
-        } catch (e) {
-          client.log(`unable to send to user ${mem.username}#${mem.discriminator}`)
+    if (guildInfo.dm_welcome_toggle && guildInfo.dm_welcome_message !== '') {
+      let msg = new client.discord.RichEmbed()
+      msg.setTitle('Welcome!')
+      msg.setDescription(guildInfo.dm_welcome_message)
+      msg.setColor(client.settings.embed_color)
+      msg.setTimestamp()
+      try {
+        mem.send(msg)
+      } catch (e) {
+        client.log(`unable to send to user ${mem.username}#${mem.discriminator}`)
+      }
+    }
+
+    if (guildInfo.welcome_toggle) {
+      if (guildInfo.welcome_channel !== '') {
+        if (guildInfo.welcome_message === '') {
+          guildInfo.welcome_message = client.settings.default_welcome
         }
-      }
 
-      if (res.welcome_toggle) {
-        if (res.welcome_channel !== '') {
-          if (res.welcome_message === '') {
-            res.welcome_message = client.settings.default_welcome
-          }
-
-          let mes = res.welcome_message.replace('{user}', `**${mem.displayName}**`)
-          client.channels.get(res.welcome_channel).send(mes).catch(e => { console.log(e) })
-        }
+        let mes = guildInfo.welcome_message.replace('{user}', `**${mem.displayName}**`)
+        client.channels.get(guildInfo.welcome_channel).send(mes)
       }
-    })
+    }
   })
 
-  client.on('guildMemberRemove', mem => {
+  client.on('guildMemberRemove', async (mem) => {
     if (mem.guild === null) {
       return
     }
 
-    client.loadGuildData(mem.guild.id, res => {
-      if (res === null || res.leave_channel === '' || !res.leave_toggle) {
-        return
-      }
+    let guildInfo = await client.loadGuildData(mem.guild.id)
+    if (guildInfo == null || guildInfo.leave_channel === '' || !guildInfo.leave_toggle) {
+      return
+    }
 
-      if (res.leave_message === '') {
-        res.leave_message = client.settings.default_leave
-      }
+    if (guildInfo.leave_message === '') {
+      guildInfo.leave_message = client.settings.default_leave
+    }
 
-      let mes = res.leave_message.replace('{user}', `**${mem.displayName}**`)
-      client.channels.get(res.leave_channel).send(mes)
-    })
+    let mes = guildInfo.leave_message.replace('{user}', `**${mem.displayName}**`)
+    client.channels.get(guildInfo.leave_channel).send(mes)
   })
 
   client.on('voiceStateUpdate', async (oldMember, newMember) => {
@@ -102,152 +100,146 @@ module.exports = client => {
     }
 
     // time handler
-    client.loadUserData(newMember.user.id, res => {
-      // auto-VC creation: create a room if all rooms are occupied. Muted or unmuted doesn't matter, because
-      // in general we want to discourage people from using rooms that are occupied even if all the participants
-      // are currently muted (one person could have been practicing there but just muted temporarily).
-      function areAllPracticeRoomsFull (restwo) {
-        let isFull = true
-        restwo.permitted_channels.forEach(chanId => {
-          let chan = client.guilds.get(newMember.guild.id).channels.get(chanId)
-          // channel being null might happen if we have a stale channel in the db - just ignore if this happens.
-          if (chan != null && chan.members.size === 0) {
-            isFull = false
-          }
-        })
+    let userInfo = await client.loadUserData(newMember.user.id)
 
-        return isFull
-      }
-
-      // auto-VC creation: remove an extra room if 1) there are at least two empty rooms and 2) one of those
-      // rooms is a temp room. (We don't want to destroy the primary rooms.)
-      function removeTempRoomIfPossible (restwo) {
-        let emptyCount = 0
-        let tempChannelToRemove = null
-        restwo.permitted_channels.forEach(chanId => {
-          let chan = client.guilds.get(newMember.guild.id).channels.get(chanId)
-          if (chan != null && chan.members.size === 0) {
-            emptyCount++
-            if (chan.name === 'Extra Practice Room') {
-              tempChannelToRemove = chan
-            }
-          }
-        })
-
-        // if tempChannelToRemove is null, it means we didn't find an empty temp channel. Don't do anything.
-        if (emptyCount >= 2 && tempChannelToRemove != null) {
-          // before removing the channel from the guild, remove it in the db.
-          restwo['permitted_channels'].splice(restwo.permitted_channels.indexOf(tempChannelToRemove.id), 1)
-          client.writeGuildData(newMember.guild.id, restwo, () => {})
-          tempChannelToRemove.delete()
+    // auto-VC creation: create a room if all rooms are occupied. Muted or unmuted doesn't matter, because
+    // in general we want to discourage people from using rooms that are occupied even if all the participants
+    // are currently muted (one person could have been practicing there but just muted temporarily).
+    function areAllPracticeRoomsFull (guildInfo) {
+      let isFull = true
+      guildInfo.permitted_channels.forEach(chanId => {
+        let chan = client.guilds.get(newMember.guild.id).channels.get(chanId)
+        // channel being null might happen if we have a stale channel in the db - just ignore if this happens.
+        if (chan != null && chan.members.size === 0) {
+          isFull = false
         }
-      }
+      })
 
-      function updatePracticeRoomChatPermissions (restwo, newMember) {
-        if (!restwo.voice_perm_toggle) {
-          return
-        }
+      return isFull
+    }
 
-        // if in any practice channel, member has rights to speak in practice room chat (can be muted)
-        let prChan = client.guilds.get(newMember.guild.id).channels.find(chan => chan.name === 'practice-room-chat')
-        if (prChan == null) {
-          client.log('Cannot find #practice-room-chat!')
-        } else if (restwo.permitted_channels.includes(newMember.voiceChannelID) && !(newMember.mute && newMember.selfDeaf)) {
-          prChan.overwritePermissions(newMember.user, { SEND_MESSAGES: true })
-        } else {
-          let existingOverride = prChan.permissionOverwrites.get(newMember.user.id)
-          // existingOverride shouldn't be null unless someone manually deletes the override, but if for some reason it's gone, no big deal, just move on.
-          if (existingOverride != null) {
-            if (existingOverride.allowed.bitfield === Discord.Permissions.FLAGS.SEND_MESSAGES && existingOverride.denied.bitfield === 0) { // the only permission was allow SEND_MESSAGES
-              existingOverride.delete()
-            } else {
-              prChan.overwritePermissions(newMember.user, { SEND_MESSAGES: null })
-            }
+    // auto-VC creation: remove an extra room if 1) there are at least two empty rooms and 2) one of those
+    // rooms is a temp room. (We don't want to destroy the primary rooms.)
+    async function removeTempRoomIfPossible (guildInfo) {
+      let emptyCount = 0
+      let tempChannelToRemove = null
+      guildInfo.permitted_channels.forEach(chanId => {
+        let chan = client.guilds.get(newMember.guild.id).channels.get(chanId)
+        if (chan != null && chan.members.size === 0) {
+          emptyCount++
+          if (chan.name === 'Extra Practice Room') {
+            tempChannelToRemove = chan
           }
         }
+      })
+
+      // if tempChannelToRemove is null, it means we didn't find an empty temp channel. Don't do anything.
+      if (emptyCount >= 2 && tempChannelToRemove != null) {
+        // before removing the channel from the guild, remove it in the db.
+        guildInfo['permitted_channels'].splice(guildInfo.permitted_channels.indexOf(tempChannelToRemove.id), 1)
+        await client.writeGuildData(newMember.guild.id, guildInfo)
+        tempChannelToRemove.delete()
+      }
+    }
+
+    function updatePracticeRoomChatPermissions (guildInfo, newMember) {
+      if (!guildInfo.voice_perm_toggle) {
+        return
       }
 
-      // if the user doesn't exist then create a user for the person
-      if (res === null) {
-        let user = {
-          'id': newMember.user.id,
-          'current_session_playtime': 0,
-          'overall_session_playtime': 0
-        }
-        client.writeUserData(newMember.user.id, user, () => {
-          client.log(`User created for ${newMember.user.username}#${newMember.user.discriminator}`)
-        })
+      // if in any practice channel, member has rights to speak in practice room chat (can be muted)
+      let prChan = client.guilds.get(newMember.guild.id).channels.find(chan => chan.name === 'practice-room-chat')
+      if (prChan == null) {
+        client.log('Cannot find #practice-room-chat!')
+      } else if (guildInfo.permitted_channels.includes(newMember.voiceChannelID) && !(newMember.mute && newMember.selfDeaf)) {
+        prChan.overwritePermissions(newMember.user, { SEND_MESSAGES: true })
       } else {
-        mutex.runExclusive(() => {
-          client.loadGuildData(newMember.guild.id, restwo => {
-            if (restwo === null) {
-              client.createGuild(newMember.guild.id)
-              client.log('Created new guild.')
-            } else {
-              updatePracticeRoomChatPermissions(restwo, newMember)
+        let existingOverride = prChan.permissionOverwrites.get(newMember.user.id)
+        // existingOverride shouldn't be null unless someone manually deletes the override, but if for some reason it's gone, no big deal, just move on.
+        if (existingOverride != null) {
+          if (existingOverride.allowed.bitfield === Discord.Permissions.FLAGS.SEND_MESSAGES && existingOverride.denied.bitfield === 0) { // the only permission was allow SEND_MESSAGES
+            existingOverride.delete()
+          } else {
+            prChan.overwritePermissions(newMember.user, { SEND_MESSAGES: null })
+          }
+        }
+      }
+    }
 
-              // run auto-VC creation logic
-              if (areAllPracticeRoomsFull(restwo)) {
-                client.guilds.get(newMember.guild.id).createChannel('Extra Practice Room', 'voice').then(newChan => {
-                  // make the new channel go in the right place
-                  let categoryChan = client.guilds.get(newMember.guild.id).channels.find(chan => chan.name === 'practice-room-chat').parent
-                  newChan.setParent(categoryChan).then(newChan => newChan.setPosition(categoryChan.children.size))
+    // if the user doesn't exist then create a user for the person
+    if (userInfo == null) {
+      userInfo = {
+        'id': newMember.user.id,
+        'current_session_playtime': 0,
+        'overall_session_playtime': 0
+      }
+      await client.writeUserData(newMember.user.id, userInfo)
+      client.log(`User created for ${newMember.user.username}#${newMember.user.discriminator}`)
+    } else {
+      mutex.runExclusive(async () => {
+        let guildInfo = await client.loadGuildData(newMember.guild.id)
+        if (guildInfo == null) {
+          await client.createGuild(newMember.guild.id)
+          client.log('Created new guild.')
+        } else {
+          updatePracticeRoomChatPermissions(guildInfo, newMember)
 
-                  // gotta update the db
-                  restwo['permitted_channels'].push(newChan.id)
-                  client.writeGuildData(newMember.guild.id, restwo, () => {})
-                })
-              } else {
-                removeTempRoomIfPossible(restwo)
-              }
+          // run auto-VC creation logic
+          if (areAllPracticeRoomsFull(guildInfo)) {
+            let newChan = await client.guilds.get(newMember.guild.id).createChannel('Extra Practice Room', 'voice')
 
-              // n.b. if this is the first time the bot sees a user, s_time may be undefined but *not* null. Therefore, == (and not ===)
-              // comparison is critical here. Otherwise, when they finished practicing, we'll try to subtract an undefined value, and we'll
-              // record that they practiced for NaN seconds. This is really bad because adding NaN to their existing time produces more NaNs.
-              if (!newMember.selfMute && !newMember.serverMute && oldMember.s_time == null && restwo.permitted_channels.includes(newMember.voiceChannelID)) {
-                // if they are unmuted and a start time dosnt exist and they are in a good channel
-                newMember.s_time = moment().unix()
-              } else if (oldMember.s_time != null) {
-                // if a start time exist transfer it to new user object
-                newMember.s_time = oldMember.s_time
-              }
+            // make the new channel go in the right place
+            let categoryChan = client.guilds.get(newMember.guild.id).channels.find(chan => chan.name === 'practice-room-chat').parent
+            newChan = await newChan.setParent(categoryChan)
+            newChan.setPosition(categoryChan.children.size)
 
-              // if user gets muted or leaves or transfers to a bad channel
-              if (newMember.voiceChannelID === null || !restwo.permitted_channels.includes(newMember.voiceChannelID) || newMember.selfMute || newMember.serverMute) {
-                if (newMember.s_time == null) {
-                  return
-                }
+            // gotta update the db
+            guildInfo['permitted_channels'].push(newChan.id)
+            await client.writeGuildData(newMember.guild.id, guildInfo)
+          } else {
+            removeTempRoomIfPossible(guildInfo)
+          }
 
-                const playtime = moment().unix() - newMember.s_time
-                res.current_session_playtime += playtime
-                res.overall_session_playtime += playtime
+          // n.b. if this is the first time the bot sees a user, s_time may be undefined but *not* null. Therefore, == (and not ===)
+          // comparison is critical here. Otherwise, when they finished practicing, we'll try to subtract an undefined value, and we'll
+          // record that they practiced for NaN seconds. This is really bad because adding NaN to their existing time produces more NaNs.
+          if (!newMember.selfMute && !newMember.serverMute && oldMember.s_time == null && guildInfo.permitted_channels.includes(newMember.voiceChannelID)) {
+            // if they are unmuted and a start time dosnt exist and they are in a good channel
+            newMember.s_time = moment().unix()
+          } else if (oldMember.s_time != null) {
+            // if a start time exist transfer it to new user object
+            newMember.s_time = oldMember.s_time
+          }
 
-                const hourrole = '529404918885384203'
-                // const activerole = '542790691617767424'
+          // if user gets muted or leaves or transfers to a bad channel
+          if (newMember.voiceChannelID === null || !guildInfo.permitted_channels.includes(newMember.voiceChannelID) || newMember.selfMute || newMember.serverMute) {
+            if (newMember.s_time == null) {
+              return
+            }
 
-                if (res.overall_session_playtime >= 40 * 60 * 60 && !newMember.roles.has(hourrole)) {
-                  newMember.addRole(hourrole)
-                    .catch(e => {
-                      client.log(`error granting user ${newMember.username} hourrole!`)
-                    })
-                    .then(() => {
-                      newMember.send('You have achieved the 40 hour pracker role!')
-                        .catch(e => {
-                          client.log('Could not tell user they leveled! (hourrole)')
-                        })
-                    })
-                }
+            const playtime = moment().unix() - newMember.s_time
+            userInfo.current_session_playtime += playtime
+            userInfo.overall_session_playtime += playtime
 
-                client.writeUserData(newMember.user.id, res, () => {
-                  client.log(`User ${newMember.user.username}#${newMember.user.discriminator} practiced for ${playtime} seconds`)
-                  newMember.s_time = null
-                  oldMember.s_time = null
-                })
+            const hourrole = '529404918885384203'
+            // const activerole = '542790691617767424'
+
+            if (userInfo.overall_session_playtime >= 40 * 60 * 60 && !newMember.roles.has(hourrole)) {
+              try {
+                await newMember.addRole(hourrole)
+                await newMember.send('You have achieved the 40 hour pracker role!')
+              } catch (err) {
+                client.log(`error awarding user ${newMember.username} the forty hour role`)
               }
             }
-          })
-        })
-      }
-    })
+
+            await client.writeUserData(newMember.user.id, userInfo)
+            client.log(`User ${newMember.user.username}#${newMember.user.discriminator} practiced for ${playtime} seconds`)
+            newMember.s_time = null
+            oldMember.s_time = null
+          }
+        }
+      })
+    }
   })
 }
