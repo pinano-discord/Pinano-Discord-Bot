09a2f55e874b33641a710e3a9d8156a0c6bae8fa
pianiststickman <34144687+pianiststickman@users.noreply.github.com>
2019-06-06 12:38:39 -0400

    Update documentation to include `p!lock` and `p!unlock` (#58)
    BUG: 21 `p!help` should return info about admin commands only to Bot Managers

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,29 +1,45 @@
 module.exports.load = (client) => {
   client.commands['help'] = {
-    run (message) {
+    async run (message) {
+      let isBotManager = message.member.roles.find('name', 'Bot Manager')
+
       let msg = new client.discord.RichEmbed()
       msg.setTitle('Help')
-      msg.setDescription(`Here is a list of all currently available commands.`)
-      msg.addField('General Commands', `
-            \`${client.settings.prefix}help\` - Show a list of available commands!
-            \`${client.settings.prefix}stats {name}\` - Show a user's practice stats! \`IE: ${client.settings.prefix}stats theProject#2997\`
-            \`${client.settings.prefix}{leaderboard/lb} {weekly/overall}\` - See the leaderboards! \`IE: ${client.settings.prefix}lb overall\`
-            \`${client.settings.prefix}info help\` - Show all available information macros!
-            `, false)
-      msg.addField('Admin Commands', `
-            \`${client.settings.prefix}settings\` - See settings commands
-            \`${client.settings.prefix}addtime {@user} {seconds}\` - Add time to a user
-            \`${client.settings.prefix}deltime {@user} {seconds}\` - Remove time from a user
-            `, false)
+      msg.addField(`\`${client.settings.prefix}help\``,
+        'Displays this help message')
+      msg.addField(`\`${client.settings.prefix}stats [username#discriminator]\``,
+        'Displays practice statistics for the specified user (default: calling user)')
+      msg.addField(`\`${client.settings.prefix}lb, ${client.settings.prefix}leaderboard [ weekly | overall ]\``,
+        'Displays the weekly or overall leaderboard (default: weekly)')
+
+      if (isBotManager) {
+        msg.addField(`\`${client.settings.prefix}unlock [<#channel_id>]\``,
+          'Unlocks the specified room for shared use (default: currently occupied room)')
+      } else {
+        msg.addField(`\`${client.settings.prefix}unlock\``,
+          'Unlocks the currently occupied room for shared use')
+      }
+
+      msg.addField(`\`${client.settings.prefix}info\``,
+        'Displays information about the Discord server')
+      msg.addField(`\`${client.settings.prefix}lock\``,
+        'Locks the currently occupied room for exclusive use')
+
+      if (isBotManager) {
+        msg.addField(`\`${client.settings.prefix}settings\``,
+          'Displays/modifies bot settings (including registering/unregistering practice rooms)')
+        msg.addField(`\`${client.settings.prefix}addtime @user [time_in_seconds]\``,
+          'Adds practice time to a user\'s record')
+        msg.addField(`\`${client.settings.prefix}deltime @user [time_in_seconds]\``,
+          'Removes practice time from a user\'s record')
+      }
+
       msg.setColor(client.settings.embed_color)
       msg.setTimestamp()
       message.author.send(msg)
-      message.reply('Sent you the command list.')
-        .then(m => {
-          setTimeout(() => {
-            m.delete()
-          }, 3000)
-        })
+
+      let m = await message.reply('sent you the command list.')
+      setTimeout(() => m.delete(), 3000)
     }
   }
 }
