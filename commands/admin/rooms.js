7d8107d249d22058b4006b2e64e59d9ce09a8d69
Eugene Chan <theproject@euge.ca>
2019-06-10 14:35:13 -0400

    Remove unused features
    Removes the `welcome`, `dm_welcome`, and `leave` features, along
    with their corresponding on/off switches in p!settings. Removes
    the `voice_perm` toggle, since we don't intend to turn it off at
    this point. The only setting that's left is `practice_channels`,
    whose function is now assumed by `p!rooms [ add | del | delete ]`.
    Also removing `p!info`; it never worked and we'll find ways outside
    of the bot to expose that information.

--- commands/admin/rooms.js
+++ commands/admin/rooms.js
@@ -1,7 +1,17 @@
+// usage:
+//
+// p!rooms
+//   - displays all registered practice rooms, their lock status, the members present
+//     in each room, their liveness status, and whether or not they are a ghost user
+// p!rooms add <#CHANNEL_ID>
+//   - registers a VoiceChannel as a practice room
+// p!rooms [del|delete] <#CHANNEL_ID>
+//   - unregisters a VoiceChannel as a practice room
+
 module.exports.load = (client) => {
   client.commands['rooms'] = {
     async run (message) {
-      if (!message.member.roles.find('name', 'Bot Manager')) {
+      if (!message.member.roles.find(r => r.name === 'Bot Manager')) {
         return client.errorMessage(message, 'You require the bot manager role to use this command.')
       }
 
@@ -10,38 +20,83 @@ module.exports.load = (client) => {
         return client.errorMessage(message, 'No data for this guild.')
       }
 
-      let msg = `Currently registered practice rooms:\n\`\`\`\n`
-      guildInfo.permitted_channels
-        .map(chanId => message.guild.channels.get(chanId))
-        .filter(chan => chan != null)
-        .sort((x, y) => x.position > y.position)
-        .forEach(chan => {
-          msg += `${chan.name}`
-          if (chan.name === 'Extra Practice Room') {
-            msg += ` (channel ID: ${chan.id})`
-          }
+      let args = message.content.split(' ').splice(1)
+      if (args.length !== 0) {
+        let usageStr = `Usage: \`${client.settings.prefix}rooms [ [ add | del | delete ] <#CHANNEL_ID> ]\``
+        if (args.length !== 2) {
+          return client.errorMessage(message, usageStr)
+        }
 
-          if (chan.locked_by != null) {
-            let occupant = chan.members.get(`${chan.locked_by}`)
-            msg += ` LOCKED by ${occupant.user.username}#${occupant.user.discriminator}`
-          }
+        if (!args[1].startsWith('<#') || !args[1].endsWith('>')) {
+          return client.errorMessage(message, usageStr)
+        }
 
-          chan.members.forEach(m => {
-            msg += `\n  - ${m.user.username}#${m.user.discriminator}`
-            if (m.deleted) {
-              msg += ` (GHOST)`
+        let chanId = args[1].replace(/[<#>]/g, '')
+        switch (args[0]) {
+          case 'add':
+          {
+            if (guildInfo['permitted_channels'].includes(chanId)) {
+              return client.errorMessage(message, `${args[1]} is already registered.`)
             }
 
-            if (m.s_time != null) {
-              msg += ` (LIVE)`
+            guildInfo['permitted_channels'].push(chanId)
+            await client.writeGuildData(message.guild.id, guildInfo)
+
+            let m = await message.reply(`added ${args[1]} to practice channels.`)
+            setTimeout(() => m.delete(), client.settings.res_destruct_time * 1000)
+            return
+          }
+          case 'del':
+          case 'delete':
+          {
+            if (!guildInfo['permitted_channels'].includes(chanId)) {
+              return client.errorMessage(message, `${args[1]} is not currently registered.`)
             }
+
+            guildInfo['permitted_channels'].splice(guildInfo.permitted_channels.indexOf(chanId), 1)
+            await client.writeGuildData(message.guild.id, guildInfo)
+
+            let m = await message.reply(`removed ${args[1]} from practice channels.`)
+            setTimeout(() => m.delete(), client.settings.res_destruct_time * 1000)
+            return
+          }
+          default:
+            return client.errorMessage(message, usageStr)
+        }
+      } else {
+        let msg = `Currently registered practice rooms:\n\`\`\`\n`
+        guildInfo.permitted_channels
+          .map(chanId => message.guild.channels.get(chanId))
+          .filter(chan => chan != null)
+          .sort((x, y) => x.position > y.position)
+          .forEach(chan => {
+            msg += `${chan.name}`
+            if (chan.name === 'Extra Practice Room') {
+              msg += ` (channel ID: ${chan.id})`
+            }
+
+            if (chan.locked_by != null) {
+              let occupant = chan.members.get(`${chan.locked_by}`)
+              msg += ` LOCKED by ${occupant.user.username}#${occupant.user.discriminator}`
+            }
+
+            chan.members.forEach(m => {
+              msg += `\n  - ${m.user.username}#${m.user.discriminator}`
+              if (m.deleted) {
+                msg += ` (GHOST)`
+              }
+
+              if (m.s_time != null) {
+                msg += ` (LIVE)`
+              }
+            })
+
+            msg += '\n'
           })
 
-          msg += '\n'
-        })
-
-      msg += `\`\`\``
-      message.channel.send(msg)
+        msg += `\`\`\``
+        message.channel.send(msg)
+      }
     }
   }
 }
