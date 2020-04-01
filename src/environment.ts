69f4e50c0706ea666c490d5a28f4e5b1ee48af9f
RGB∆ <abgier.avraha@gmail.com>
2020-04-02 00:12:20 +1100

    default env fix

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  command_prefix: process.env.COMMAND_PREFIX ?? 'p!',
   token: process.env.BOT_TOKEN ?? '',
+  command_prefix: process.env.COMMAND_PREFIX ?? 'p!',
   default_bitrate: process.env.BITRATE ?? 384,
-  mongo_url: process.env.MONGO_URL ?? 'mongodb://localhost:27017',
+  mongo_url: process.env.MONGO_URL ?? 'mongodb://user:password@localhost:27017',
   mong_db_name: process.env.MONGO_DB_NAME ?? 'db',
   embed_color: process.env.EMBED_COLOR ?? 16752128,
   voice_channel_category: process.env.VOICE_CHANNEL_CATEGORY ?? 'practice-rooms',
